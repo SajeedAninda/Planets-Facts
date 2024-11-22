ce586e2e7d42d7d3889e6b27aafdd0a15981aae2
SajeedAninda <sajaninda@gmail.com>
2024-11-22 17:17:32 +0600

    Completed Navbar with hover animation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,22 +10,47 @@ const Navbar = () => {
             .catch((error) => console.error('Error fetching data:', error));
     }, []);
 
+    const colors = {
+        Mercury: '#58a6ff',
+        Venus: '#ffac3b',
+        Earth: '#8358ff',
+        Mars: '#ff6a3b',
+        Jupiter: '#e57300',
+        Saturn: '#ffbf47',
+        Uranus: '#50d7c2',
+        Neptune: '#3b89ff',
+    };
+
     return (
-        <div className='bg-[#070724] h-[13vh] border-b border-gray-400 flex items-center'>
-            <div className='w-[95%] mx-auto flex justify-between items-center'>
+        <div className="bg-[#070724] h-[13vh] border-b border-gray-400 flex items-center">
+            <div className="w-[95%] mx-auto flex justify-between items-center">
                 <div className="logo">
-                    <p className='text-[28px] text-white font-antonio font-semibold'>
+                    <p className="text-[28px] text-white font-antonio font-semibold">
                         THE PLANETS
                     </p>
                 </div>
 
-                <div className="menu flex gap-16">
+                <div className="menu flex gap-12">
                     {items?.map((item) => (
-                        <div key={item.name} className="menu-item uppercase font-spartan text-[13px] text-[#bfc2c7] font-bold tracking-wider cursor-pointer hover:text-white transition duration-75 ease-in-out">
-                            {item?.name}
+                        <div
+                            key={item.name}
+                            className="relative group flex-1 text-center uppercase font-spartan text-[13px] text-[#bfc2c7] font-bold tracking-wider cursor-pointer transition duration-75 ease-in-out"
+                        >
+                            <div
+                                className="absolute inset-x-0 -top-8 h-[5px] w-0 bg-transparent group-hover:w-full group-hover:bg-current transition-all duration-300 ease-in-out"
+                                style={{
+                                    backgroundColor: colors[item.name] || '#bfc2c7',
+                                }}
+                            ></div>
+
+                            <span className="relative z-10 group-hover:text-white">
+                                {item.name}
+                            </span>
                         </div>
                     ))}
                 </div>
+
+
             </div>
         </div>
     );
