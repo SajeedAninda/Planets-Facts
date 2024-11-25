3b526d4608f7c6bb0f756bfa82b556171a35d908
SajeedAninda <sajaninda@gmail.com>
2024-11-25 14:48:29 +0600

    Lifted the colors in app.jsx to share between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
-import Navbar from "./Components/Navbar/Navbar";
+import { useState } from "react";
 import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
     const [selectedPlanet, setSelectedPlanet] = useState("Earth");
+    const colors = {
+        Mercury: '#58a6ff',
+        Venus: '#ffac3b',
+        Earth: '#6d2ed5',
+        Mars: '#ff6a3b',
+        Jupiter: '#e57300',
+        Saturn: '#ffbf47',
+        Uranus: '#50d7c2',
+        Neptune: '#3b89ff',
+    };
+    const selectedColor = colors[selectedPlanet] || "#6d2ed5";
 
     return (
         <>
-            <Navbar onPlanetChange={setSelectedPlanet} />
-            <Home selectedPlanet={selectedPlanet} />
+            <Navbar
+                selectedPlanet={selectedPlanet}
+                setSelectedPlanet={setSelectedPlanet}
+            />
+            <Home selectedPlanet={selectedPlanet} buttonColor={selectedColor} />
         </>
     );
 }
